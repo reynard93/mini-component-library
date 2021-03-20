0343a7ed1c3dd6cf1e00808ecd9e2f43d9910ca7
reynard <reynard93@gmail.com>
2021-03-20 17:44:56 +0800

    finish iconinput comp

--- src/components/IconInput/IconInput.js
+++ src/components/IconInput/IconInput.js
@@ -6,14 +6,83 @@ import { COLORS } from '../../constants';
 import Icon from '../Icon';
 import VisuallyHidden from '../VisuallyHidden';
 
+const STYLES = {
+  small: {
+    fontSize: 14,
+    iconSize: 16,
+    borderThickness: 1,
+    height: 24,
+  },
+  large: {
+    fontSize: 18,
+    iconSize: 24,
+    borderThickness: 2,
+    height: 36,
+  },
+};
+
 const IconInput = ({
   label,
   icon,
   width = 250,
   size,
   placeholder,
+  ...delegated
 }) => {
-  return 'TODO';
+  const styles = STYLES[size];
+
+  return (
+    <Wrapper>
+      <VisuallyHidden>{label}</VisuallyHidden>
+      <IconWrapper style={{ '--size': styles.iconSize + 'px' }}>
+        <Icon id={icon} size={styles.iconSize} />
+      </IconWrapper>
+      <TextInput
+        {...delegated}
+        style={{
+          '--width': width + 'px',
+          '--height': styles.height / 16 + 'rem',
+          '--border-thickness': styles.borderThickness + 'px',
+          '--font-size': styles.fontSize / 16 + 'rem'
+        }}
+      />
+    </Wrapper>
+  );
 };
+
+const Wrapper = styled.label`
+  display: block;
+  position: relative;
+  color: ${COLORS.gray700};
+
+  &:hover {
+    color: ${COLORS.black};
+  }
+`;
+
+const TextInput = styled.input`
+  height: var(--height); /*${24 / 16}rem*/
+  width: var(--width);
+  font-size: var(--font-size); /*${14 / 16}rem*/
+  border: none;
+  border-bottom: var(--border-thickness) solid ${COLORS.black};
+  padding-left: var(--height);
+  font-weight: 700;
+  outline-offset: 2px;
+  color: inherit;
+  
+  &::placeholder {
+    color: ${COLORS.gray500}
+    font-weight: 400;
+  }
+`;
+
+const IconWrapper = styled.div`
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  margin: auto 0;
+  height: var(--size);
+`;
 
 export default IconInput;
